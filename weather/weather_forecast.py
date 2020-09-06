51150474ad51135a72fad3712e444f20974f96e8
Daniel Sarno <dansarno91@gmail.com>
2020-09-07 00:01:19 +0100

    Decided to use minutely, hourly and daily forecasts from OpenWeather as each provides useful information for the bot

--- weather/weather_forecast.py
+++ weather/weather_forecast.py
@@ -4,17 +4,30 @@ import requests
 import json
 
 
-def fetch_hourly_forecast(isLocal, location=(51.5074, 0.1278)):
+def fetch_forecast(isLocal, location=(51.5074, 0.1278)):
     if not isLocal:
         api_key = os.getenv("API_KEY")
     else:
         api_key = cred.API_KEY
 
     lat, lon = location
-    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,daily&appid={api_key}"
+    url = (f"https://api.openweathermap.org/data/2.5/onecall?"
+           f"lat={lat}&lon={lon}"
+           f"&exclude=current"
+           f"&&units=metric"
+           f"&appid={api_key}")
+
     response = requests.get(url)
+    full_response_dict = response.json()
+    minutely_forecast = full_response_dict["minutely"]
+    hourly_forecast = full_response_dict["hourly"]
+    daily_forecast = full_response_dict["daily"]
 
-    return response.json()
+    return minutely_forecast, hourly_forecast, daily_forecast
 
 
-print(fetch_hourly_forecast(True))
+def parse_hourly_forecast(hourly_forecast):
+    pass
+
+
+minutes, hours, days = fetch_forecast(True)
