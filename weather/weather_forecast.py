2481a3539aa119ae51cfbea0f08e413e17553415
Daniel Sarno <dansarno91@gmail.com>
2020-09-08 09:53:21 +0100

    filter_forecasts now also returns the next day forecast

--- weather/weather_forecast.py
+++ weather/weather_forecast.py
@@ -26,16 +26,29 @@ def fetch_forecast(isLocal, location=(51.5074, 0.1278)):
     return hourly_forecast, daily_forecast
 
 
-def group_forecasts_in_windows(hourly_forecast, time_windows):
+def filter_forecasts(hourly_forecast, daily_forecast, time_windows):
+    """ Filters all the hourly and daily forecasts returned from the API call to just those
+    in the next day time windows.
+
+    :param hourly_forecast: 48 hour forecast from API call
+    :type hourly_forecast: List of dictionaries
+    :param daily_forecast: 8 day forecast from API call
+    :type daily_forecast: List of dictionaries
+    :param time_windows: Start and end times of the running time windows
+    :type time_windows: Dictionary
+    :return: Morning, midday, evening and next day forecasts
+    :rtype: List of dictionaries
+    """
+
     morning_forecast = []
     midday_forecast = []
-    afternoon_forecast = []
+    evening_forecast = []
     for hour_forecast in hourly_forecast:
         this_datetime = datetime.fromtimestamp(hour_forecast["dt"])
         is_next_day = datetime.now().day + 1 == this_datetime.day
         is_in_morning = time_windows["morning"][0].hour <= this_datetime.hour <= time_windows["morning"][1].hour
         is_in_midday = time_windows["midday"][0].hour <= this_datetime.hour <= time_windows["midday"][1].hour
-        is_in_afternoon = time_windows["afternoon"][0].hour <= this_datetime.hour <= time_windows["afternoon"][1].hour
+        is_in_afternoon = time_windows["evening"][0].hour <= this_datetime.hour <= time_windows["evening"][1].hour
         if not is_next_day:
             continue
         if is_in_morning:
@@ -43,8 +56,12 @@ def group_forecasts_in_windows(hourly_forecast, time_windows):
         if is_in_midday:
             midday_forecast.append(hour_forecast)
         if is_in_afternoon:
-            afternoon_forecast.append(hour_forecast)
-    return morning_forecast, midday_forecast, afternoon_forecast
+            evening_forecast.append(hour_forecast)
+
+    next_day_forecast = [day_forecast for day_forecast in daily_forecast
+                         if datetime.fromtimestamp(day_forecast["dt"]).day == datetime.now().day + 1][0]
+
+    return morning_forecast, midday_forecast, evening_forecast, next_day_forecast
 
 
 def aggregate_forecast(hourly_forecast):
@@ -52,8 +69,12 @@ def aggregate_forecast(hourly_forecast):
 
 
 hours, days = fetch_forecast(True)
-windows_dict = {
+TIME_WINDOWS = {
     "morning": [time(hour=6), time(hour=9)],
     "midday": [time(hour=12), time(hour=14)],
-    "afternoon": [time(hour=17), time(hour=21)]
-    }
+    "evening": [time(hour=17), time(hour=21)]
+}
+
+TEMPERATURE_WEIGHTINGS = {
+
+}
