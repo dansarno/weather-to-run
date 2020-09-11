e66e8edb9d8dd15847a769e0070b1b2d87431081
Daniel Sarno <dansarno91@gmail.com>
2020-09-11 13:24:01 +0100

    Now average and set attributes of the segments

--- weather/weather.py
+++ weather/weather.py
@@ -28,6 +28,8 @@ class Day(TimePeriod):
         self.segments = [DaySegment(name, times[0], times[1]) for name, times in segments.items()]
         self.rankings = {"Green": [], "Amber": [], "Red": []}
 
+        self.add_forecast()
+
     def __str__(self):
         return f"Day with date {self.date.strftime('%d/%m/%y')} and {len(self.segments)} segments: " \
                f"{', '.join([segment.name.title() for segment in self.segments])}"
@@ -50,6 +52,8 @@ class Day(TimePeriod):
             self.hours.append(this_hour)
             day_temps.append(hour_forecast["feels_like"])
 
+        self._filter_forecast()
+
         for day_forecast in daily_forecasts:
             is_this_day = self.date == datetime.datetime.fromtimestamp(day_forecast["dt"]).date()
             if not is_this_day:
@@ -66,6 +70,9 @@ class Day(TimePeriod):
             hour.wind_score = fcast.wind_speed_to_score(hour.wind_mps)
             hour.precipitation_score = precip_scores_dict[hour.precipitation_type]
 
+        for segment in self.segments:
+            segment.average_score()
+
     def _filter_forecast(self):
         for hour in self.hours:
             # Check if this hour is within any of the time segments
@@ -73,6 +80,10 @@ class Day(TimePeriod):
                 is_in_window = segment.start_time.hour <= hour.hr <= segment.end_time.hour
                 if is_in_window:
                     segment.hours.append(hour)
+
+        # Update segment attributes after segments have hours
+        for segment in self.segments:
+            segment.average_weather()
 
     def weather_at_time(self, time):
         pass
@@ -91,10 +102,34 @@ class DaySegment(TimePeriod):
         return f"DaySegment object called {self.name.title()} ({self.start_time} to {self.end_time})"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name.title()} ({self.start_time} - {self.end_time}))"
+        return f"{self.__class__.__name__}({self.name.title()}({self.start_time} - {self.end_time}))"
+
+    def average_weather(self):
+        all_temp_c = [hour.temp_c for hour in self.hours]
+        all_wind_mps = [hour.wind_mps for hour in self.hours]
+        all_precip_types = [hour.precipitation_type for hour in self.hours]
+
+        self.temp_c = round(sum(all_temp_c) / len(all_temp_c), 2)
+        self.wind_mps = round(sum(all_wind_mps) / len(all_wind_mps), 2)
+        self.precipitation_type = all_precip_types[len(all_precip_types) // 2]  # this may need changing!
+
+    def average_score(self):
+        all_temp_scores = [hour.temp_score for hour in self.hours]
+        all_wind_scores = [hour.wind_score for hour in self.hours]
+        all_precip_scores = [hour.precipitation_score for hour in self.hours]
+
+        self.temp_score = round(sum(all_temp_scores) / len(all_temp_scores), 2)
+        self.wind_score = round(sum(all_wind_scores) / len(all_wind_scores), 2)
+        self.precipitation_score = all_precip_scores[len(all_precip_scores) // 2]  # this may need changing!
 
 
 class Hour(TimePeriod):
     def __init__(self, hr):
         super().__init__()
         self.hr = hr
+
+    def __str__(self):
+        return f"Hour object at {self.hr:02}:00"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.hr:02})"
