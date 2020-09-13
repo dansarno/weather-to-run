edf0ff92f67aed1f53ff1c2aeefa3ebbe2e7119b
Daniel Sarno <dansarno91@gmail.com>
2020-09-13 23:01:14 +0100

    Still forming the initial workings of the tweet generator, replacing "NOUN" with the results of the weather scripts

--- bots/tweet_composer.py
+++ bots/tweet_composer.py
@@ -2,11 +2,28 @@ import yaml
 import numpy as np
 import random
 
+
+def add_selections_to_tweet(tweet_text, selections):
+    tweet_words = tweet_text.split(" ")
+    correct_words = []
+    choice = 0
+    for word in tweet_words:
+        if word == "NOUN":
+            correct_words.append(selections[choice])
+            if len(selections) > 1:
+                choice += 1
+        else:
+            correct_words.append(word)
+    formed_tweet = " ".join(correct_words)
+    return formed_tweet
+
+
 with open('tweet_content.yaml', 'r', encoding="utf8") as f:
     doc = yaml.load(f)
 
 tone = "Green"
-weathers = 2
+results = ["morning"]
+weathers = len(results)
 prob_of_no_intro = 0.3
 prob_of_no_outro = 0.25
 
@@ -15,10 +32,12 @@ for _ in range(100):
     middle = random.choice(doc[f'Weather text {weathers}'][tone])
     end = random.choice(doc['Outro'][tone])
 
+    formed_middle = add_selections_to_tweet(middle, results)
+
     tweet_construction = []
     if random.random() > prob_of_no_intro:
         tweet_construction.append(start)
-    tweet_construction.append(middle)
+    tweet_construction.append(formed_middle)
     if random.random() > prob_of_no_outro:
         tweet_construction.append(end)
 
