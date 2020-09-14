e5cc3dc623d7c051795df0ae7816a5606167f401
Daniel Sarno <dansarno91@gmail.com>
2020-09-14 21:26:44 +0100

    Now works with multiple selections using the count argument

--- bots/tweet_composer.py
+++ bots/tweet_composer.py
@@ -5,22 +5,10 @@ import re
 
 
 def add_selections_to_tweet(tweet_text, selections):
-    tweet_words = tweet_text.split(" ")
-    correct_words = []
-    choice = 0
-    for word in tweet_words:
-        if word == "NOUN":
-            correct_words.append(selections[choice])
-            if len(selections) > 1:
-                choice += 1
-        else:
-            correct_words.append(word)
-    formed_tweet = " ".join(correct_words)
-    return formed_tweet
+    text_with_selection = tweet_text
+    for selection in selections:
+        text_with_selection = re.sub("NOUN", selection, text_with_selection, count=1)
 
-
-def add_selections_to_tweet2(tweet_text, selections):
-    text_with_selection = re.sub("NOUN", selections[0], tweet_text)
     if selections[0] == "afternoon" or selections[0] == "evening":
         text_with_selection_and_indef = re.sub("A\(N\)", "an", text_with_selection)
     else:
@@ -31,8 +19,8 @@ def add_selections_to_tweet2(tweet_text, selections):
 with open('tweet_content.yaml', 'r', encoding="utf8") as f:
     doc = yaml.load(f)
 
-tone = "Amber"
-results = ["afternoon"]
+tone = "Green"
+results = ["afternoon", "morning"]
 weathers = len(results)
 prob_of_no_intro = 0.3
 prob_of_no_outro = 0.25
@@ -42,7 +30,7 @@ for _ in range(100):
     middle = random.choice(doc[f'Weather text {weathers}'][tone])
     end = random.choice(doc['Outro'][tone])
 
-    formed_middle = add_selections_to_tweet2(middle, results)
+    formed_middle = add_selections_to_tweet(middle, results)
 
     tweet_construction = []
     if random.random() > prob_of_no_intro:
