5a6a67e610f1141b9cc449bce871ac0fa5c136a5
Daniel Sarno <dansarno91@gmail.com>
2020-09-18 23:59:36 +0100

    Major clear-out of forecast.py

--- weather/forecast.py
+++ weather/forecast.py
@@ -34,68 +34,6 @@ def fetch_forecast(is_local, location=(51.5074, 0.1278)):
     return hourly_forecast, daily_forecast
 
 
-def filter_forecasts(hourly_forecast, daily_forecast, time_windows):
-    """ Filters all the hourly and daily forecasts returned from the API call to just those
-    in the next day time windows.
-
-    :param hourly_forecast: 48 hour forecast from API call
-    :type hourly_forecast: List of dictionaries
-    :param daily_forecast: 8 day forecast from API call
-    :type daily_forecast: List of dictionaries
-    :param time_windows: Start and end times of the running time windows
-    :type time_windows: Dictionary
-    :return: Morning, afternoon, evening and next day forecasts
-    :rtype: List of dictionaries
-    """
-
-    windowed_forecasts = {window_name: [] for window_name in time_windows.keys()}
-    for hour_forecast in hourly_forecast:
-        this_datetime = datetime.datetime.fromtimestamp(hour_forecast["dt"])
-        is_next_day = datetime.datetime.now().day + 1 == this_datetime.day
-        if not is_next_day:
-            continue
-
-        # Check if this hour is within any of the time windows
-        for window_name, window_times in time_windows.items():
-            is_in_window = window_times[0].hour <= this_datetime.hour <= window_times[1].hour
-            if is_in_window:
-                windowed_forecasts[window_name].append(hour_forecast)
-
-    # TODO datetime.now() may be problematic for the deployed app
-    tomorrows_summary = [day_forecast for day_forecast in daily_forecast
-                         if datetime.datetime.fromtimestamp(day_forecast["dt"]).day == datetime.datetime.now().day + 1][
-        0]
-
-    return windowed_forecasts, tomorrows_summary
-
-
-def aggregate_scores(hourly_forecast, what_to_score):
-    all_scores = [score_forecast(hour_forecast, what_to_score) for hour_forecast in hourly_forecast]
-    lowest_scores_and_reasons = [(min(score), score.index(min(score))) for score in all_scores]
-    return lowest_scores_and_reasons
-
-
-def score_window_and_why(lowest_scores_and_reasons, weather_parameters):
-    lowest_score = 10
-    worst_parameter = ""
-    for score, reason in lowest_scores_and_reasons:
-        if score < lowest_score:
-            lowest_score = score
-            worst_parameter = weather_parameters[reason]
-    return lowest_score, worst_parameter
-
-
-def score_forecast(hour_forecast, what_to_score):
-    all_scores = []
-    if "temperature" in what_to_score:
-        all_scores.append(temp_c_to_score(hour_forecast["feels_like"]))
-    if "wind" in what_to_score:
-        all_scores.append(wind_speed_to_score(hour_forecast["wind_speed"]))
-    if "precipitation" in what_to_score:
-        all_scores.append(config.PRECIPITATION_SCORES[str(int(hour_forecast["weather"][0]["id"]))])
-    return all_scores
-
-
 def wind_speed_to_score(wind_speed):
     # Empirical score (9-best, 0-worst) based off the Beaufort scale
     score = 10 - ((wind_speed ** (7 / 6)) / 6)
@@ -220,23 +158,6 @@ def _plot_scores(hourly_forecast, what_to_score, time_windows):
     ax5.spines['top'].set_visible(False)
 
     plt.show()
-
-
-# def when_to_run(time_windows, is_local=True, is_debug=True):
-#     hourly_forecast, daily_forecast = fetch_forecast(is_local)
-#     if is_debug:
-#         _plot_scores(hourly_forecast, config.WEATHER_PARAMETERS, time_windows)
-#     windowed_forecasts, tomorrows_summary = filter_forecasts(hourly_forecast, daily_forecast, time_windows)
-#     highest_score = -1
-#     best_time = ""  # likely need changing
-#     for window_name, forecast in windowed_forecasts.items():
-#         this_windows_score = score_window_and_why(
-#             aggregate_scores(forecast, config.WEATHER_PARAMETERS), config.WEATHER_PARAMETERS)[0]
-#         is_a_better_time = highest_score < this_windows_score
-#         if is_a_better_time:
-#             highest_score = this_windows_score
-#             best_time = window_name
-#     return best_time
 
 
 if __name__ == "__main__":
