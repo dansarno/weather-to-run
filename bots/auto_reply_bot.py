119976c868ddcf76bcf8ace5cb7c282387a1fe13
Daniel Sarno <dansarno91@gmail.com>
2020-09-29 10:29:43 +0100

    Auto reply bot documented

--- bots/auto_reply_bot.py
+++ bots/auto_reply_bot.py
@@ -9,6 +9,18 @@ def create_geocoder_obj():
 
 
 def text_to_location(tweet_text):
+    """Generates a location dictionary (city name and coordinates) given a sentence string.
+
+    Uses geotext package to search a string for recognised city names contained within it. Then uses the opencage
+    package to retrieve the latitude and longitude coordinates of the given city and forms a dictionary of the city
+    name (key) and a tuple of the coordinates (value)
+
+    Args:
+        tweet_text (str): Sentence string
+
+    Returns:
+        Location dict or None: If a recognised city is in the tweet_text, a location is returned. Else, return None
+    """
     geo = create_geocoder_obj()
 
     city_list = GeoText(tweet_text).cities
