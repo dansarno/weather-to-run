fdc4d2f4d35b3d013080cc9e9ded209288f29943
Daniel Sarno <dansarno91@gmail.com>
2020-10-15 12:45:30 +0100

    Added proper connect and disconnect methods, will transition to sqlalchemy

--- bots/tweetdb.py
+++ bots/tweetdb.py
@@ -4,9 +4,20 @@ import os
 
 class TweetData:
     def __init__(self):
-        self.conn = self._connect()
+        self.conn = None
+        self.cursor = None
 
-    @staticmethod
-    def _connect():
-        conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='require')
-        return conn
+        self.connect()
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(os.getenv("PG_DB_URI"), sslmode='require')
+            self.cursor = self.conn.cursor()
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
+    def disconnect(self):
+        if self.conn:
+            self.cursor.close()
+            self.conn.close()
+            print("PostgreSQL connection is closed")
